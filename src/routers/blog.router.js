2d053fa26682629f3854e9927d0e7fb5060ca21a
Xander <xander@vinova.com.sg>
2021-06-22 16:13:07 +0700

    remove files unused

--- src/routers/blog.router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-import BogController from "../controllers/blog.controller";
-
-router.get("/", BogController.getList);
-router.post("/", BogController.create);
-
-export default router;
--- src/routers/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import blogRouter from "./blog.router";
-const router = Router();
-
-router.use("/blog", blogRouter);
-
-export default router;
